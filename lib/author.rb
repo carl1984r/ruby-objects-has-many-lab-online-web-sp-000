8cb4f95d8ff5ac2be0e6fb4dbc83c5b13f25328a
Bry Cox <carl.cc@gmail.com>
2019-02-24 23:38:26 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,29 @@
+class Author
+
+  attr_accessor :name, :author
+
+def initialize(name)
+  @name = name
+  @posts = []
+end
+
+def add_post(title)
+  @posts << title
+  title.author = self
+end
+
+def add_post_by_title(title)
+  post = Post.new(title)
+  post.author = self
+  @posts << post
+end
+
+def posts
+  @posts
+end
+
+
+
+
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,19 @@
+require 'pry'
+class Song
+
+  @@all = []
+
+  attr_accessor :artist, :name
+
+  def initialize(name)
+    @name = name
+    @@all << self
+    end
+
+  def self.all
+    @@all
+  end
+
+
+
+end
